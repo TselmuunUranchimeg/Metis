7cca6fe45f1bf77332e588dc9d7336f00a79d9f3
Tselmuun Uranchimeg <tselmuunu@gmail.com>
2023-06-21 16:42:19 +0800

    CSS custom class

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,9 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      gridTemplateColumns: {
+        'custom': "repeat(auto-fill, minmax(270px, 1fr))"
+      }
     },
   },
   plugins: [],
